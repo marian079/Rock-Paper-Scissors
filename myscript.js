bb6a3fc9e5216e798f6ed2858cb605ecc7f0e230
Davidov Marian <davidov_marian@yahoo.ro>
2022-06-13 11:51:40 +0300

    finish the project

--- myscript.js
+++ myscript.js
@@ -43,8 +43,7 @@ function computerPlay() {
         }
             else if(playerSelection === computerSelection)
             {
-                user_score++;
-                computer_score++;
+                user_score = 0, computer_score = 0;
             response = (result(1, 1));
             }
             else if(playerSelection === 'scissors' && computerSelection === 'rock')
@@ -72,24 +71,33 @@ function computerPlay() {
                             computer_score++;
                         response = (result(0, 1) + computerSelection + ' beats ' + playerSelection);
                         }
-
                         return response;
+                        
 
-                    }
-    let final_score_user = 0, final_score_computer;
+    }
+    
+                    
+    let final_score_user = 0, final_score_computer = 0;
     function game()
     {
-        for(let i = 1; i <= 5; ++i)
-        {
-            playRound(playerSelection, computerSelection);
-             console.log(playRound(playerSelection, computerSelection));
+            for(let i = 1; i <= 5; ++i)
+        {   computer_score = 0, user_score = 0;
+            playerSelection =  prompt('What you chose, a rock, a paper or scissors?');
+            if(playerSelection === null || playerSelection == 'stop')
+            break;
+           console.log(playRound(playerSelection, computerSelection));   
+         
             if(computer_score)
             final_score_computer++;
-            else if(computer_score && user_score)
-            final_score_computer++, final_score_computer++;
             else 
                 final_score_user++;
         }
-        console.log(game());
+        if(final_score_computer > final_score_user)
+        return "Computer win!" + final_score_computer + ' - ' + final_score_user;
+        else 
+            return "You win!" + final_score_user + ' - ' + final_score_computer; 
     }
-   
+    console.log(game());
+
+   playerSelection =  prompt('What you choose, a rock, a paper or scissors?', 'stop');
+      
